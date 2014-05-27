d3fd9d5e239c160001e9a41a16c658f92c27ebd8
Feialoh Francis <feialohfrancis@gmail.com>
2014-05-27 14:33:32 +0530

    Modified path

---
+++ EmailRecipientFillTests/EmailRecipientFillTests.m
@@ -0,0 +1,34 @@
+//
+//  EmailRecipientFillTests.m
+//  EmailRecipientFillTests
+//
+//  Created by feialoh on 27/05/14.
+//  Copyright (c) 2014 Cabot. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmailRecipientFillTests : XCTestCase
+
+@end
+
+@implementation EmailRecipientFillTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
